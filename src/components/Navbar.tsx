dad7764f36a2397158c6ae78d44ea2702934ee24
SinqoMas <sinqomas@gmail.com>
2024-09-11 19:17:57 +0200

    $ git checkout -b main origin/main fatal: 'origin/main' is not a commit and a branch 'main' cannot be created from it
    update of my work on the online store#

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+const NavBar = () => {
+    return (
+        <div className='h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64'>
+            {/* MOBILE */}
+            /*<link href="">LAMA</link>*/
+        </div>
+    )
+}
+
+export default NavBar
